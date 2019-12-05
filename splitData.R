2252bfa4fd4cc49167ab1aa69fc00965e3d681e1
Cathy Cha <ccha@md4384-ccha.ad.oicr.on.ca>
2019-12-05 15:20:23 -0500

    split and save data

--- splitData.R
+++ splitData.R
@@ -4,5 +4,11 @@ load("../SB_Data/phasebatch.Rdata")
 # split() dataframe into 500 slices and stored as a large list of dataframes 
 splitted <- split(phasebatch, sample(rep(1:500, 26500)))
 
-#save as RData
-save(splitted, file="splitted.Rdata")
+# save slices into separate Rdatas 
+for (i in 1:length(splitted)) {
+  slice <- splitted[[i]]
+  save(slice, file=paste0("slice", i, ".Rdata"))
+}
+
+# #save as RData
+# save(splitted, file="splitted.Rdata")
