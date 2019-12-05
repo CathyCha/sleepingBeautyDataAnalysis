9b0deeb3be3f8f5eec06060f570f79a9b5c78c62
Cathy Cha <ccha@md4384-ccha.ad.oicr.on.ca>
2019-12-05 14:19:12 -0500

    added slice splits

--- getRefSeq.R
+++ getRefSeq.R
@@ -1,3 +1,10 @@
+
+# Load the data 
+load("../SB_Data/phasebatch.Rdata")
+
+# split() dataframe into 500 slices and stored as a large list of dataframes 
+splitted <- split(phasebatch, sample(rep(1:500, 26500)))
+
 getRefSeq <- function(sb){
   for (i in 1:length(sb$ref)) {
     sb$ref[i] <- toString(getSeq(BSgenome.Mmusculus.UCSC.mm9, GRanges(sb$Chr[i], IRanges(sb$Location[i], sb$Location.1[i]))))
