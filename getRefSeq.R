25d6da8098200a7ce083e3e0d613a659bb47e372
Cathy Cha <ccha@md4384-ccha.ad.oicr.on.ca>
2019-12-05 12:58:35 -0500

    getRefSeq function

---
+++ getRefSeq.R
@@ -0,0 +1,5 @@
+getRefSeq <- function(sb){
+  for (i in 1:length(sb$ref)) {
+    sb$ref[i] <- toString(getSeq(BSgenome.Mmusculus.UCSC.mm9, GRanges(sb$Chr[i], IRanges(sb$Location[i], sb$Location.1[i]))))
+  }
+} 
