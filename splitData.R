4d8ebb319525c1d146087c24f52b8e26d54d22f4
Cathy Cha <ccha@md4384-ccha.ad.oicr.on.ca>
2019-12-06 16:47:40 -0500

    change variable names

--- splitData.R
+++ splitData.R
@@ -1,8 +1,8 @@
 # Load the data 
-load("../SB_Data/phasebatch.Rdata")
+load("../SB_Data/combinedAnnotParsed.Rdata")
 
 # split() dataframe into 500 slices and stored as a large list of dataframes 
-splitted <- split(phasebatch, sample(rep(1:500, 26500)))
+splitted <- split(puttogether, sample(rep(1:500, 26700)))
 
 # save slices into separate Rdatas 
 for (i in 1:length(splitted)) {
