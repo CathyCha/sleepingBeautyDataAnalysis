33a157e50772e8c7621a8bf840e199b5c95b5d10
Cathy Cha <ccha@md4384-ccha.ad.oicr.on.ca>
2019-12-06 15:02:20 -0500

    fix

--- getRefSeq.R
+++ getRefSeq.R
@@ -1,9 +1,16 @@
+library(parallel)
+library(BSgenome)
+library(GenomicRanges)
+library(BSgenome.Mmusculus.UCSC.mm9)
+library(IRanges)
+
+
 #add ref seq to data frame 
 getRefSeq <- function(slice){
   for (i in 1:length(slice$ref)) {
-    slice$ref[i] <- toString(getSeq(BSgenome.Mmusculus.UCSC.mm9, GRanges(slice$Chr[i], IRanges(slice$Location[i], slice$Location.1[i]))))
+    slice$ref[i] <- toString(BSgenome::getSeq(BSgenome.Mmusculus.UCSC.mm9, GRanges(slice$Chr[i], IRanges(slice$Location[i], slice$Location.1[i]))))
   }
-  save(slice, file=paste0("../finishedSB_Data" , slice, ".Rdata"))
+  return(slice)
 } 
 
 #read in individual slice specified by cluster job
@@ -14,5 +21,8 @@ mybasenm <- basename(myfn)
 print(mybasenm)
 myfn <- get(load(myfn))
 print(typeof(myfn))
+print(head(myfn))
 
-getRefSeq(myfn)
+result <- getRefSeq(myfn)
+save(result, file=paste0("../finishedSB_Data/", mybasenm)
+
