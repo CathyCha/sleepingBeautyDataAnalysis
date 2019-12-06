8b90e8be9d484318e667e423204492446ab2ea90
Cathy Cha <ccha@md4384-ccha.ad.oicr.on.ca>
2019-12-06 12:47:09 -0500

    added command script

--- getRefSeq.R
+++ getRefSeq.R
@@ -1,6 +1,17 @@
 #add ref seq to data frame 
-getRefSeq <- function(sb){
-  for (i in 1:length(sb$ref)) {
-    sb$ref[i] <- toString(getSeq(BSgenome.Mmusculus.UCSC.mm9, GRanges(sb$Chr[i], IRanges(sb$Location[i], sb$Location.1[i]))))
+getRefSeq <- function(slice){
+  for (i in 1:length(slice$ref)) {
+    slice$ref[i] <- toString(getSeq(BSgenome.Mmusculus.UCSC.mm9, GRanges(slice$Chr[i], IRanges(slice$Location[i], slice$Location.1[i]))))
   }
 } 
+
+#read in individual slice specified by cluster job
+myfn <- commandArgs(trailingOnly=TRUE)[1]
+print("done loading")
+print(myfn)
+mybasenm <- basename(myfn)
+print(mybasenm)
+slice <- load(paste0("../SB_Data/", myfn))
+
+
+# getRefSeq(slice)
