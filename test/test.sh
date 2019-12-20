dc40e216ea1e9ba400af66c74989fa9c8e7c9716
Cathy <cathy455503@gmail.com>
2019-12-20 15:48:24 -0800

    test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,4 @@
 module load R/3.6.0;
-qsub -cwd -N "test" -l h_vmem=130g "LD_PRELOAD=/oicr/local/sw/gcc-6.5.0/lib64/libstdc++.so.6; Rscript /u/ccha/sleepingBeautyDataAnalysis/test/test.r"
-    
+LD_PRELOAD=/oicr/local/sw/gcc-6.5.0/lib64/libstdc++.so.6;
+qsub -cwd -N "test" -l h_vmem=130g "Rscript /u/ccha/sleepingBeautyDataAnalysis/test/test.r"
+  
