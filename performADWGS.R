a54e4a4eea19c2c49a6e8d2159acd8d01334716b
Cathy <cathy455503@gmail.com>
2019-12-20 20:52:58 -0800

    perform parallel

--- performADWGS.R
+++ performADWGS.R
@@ -11,13 +11,14 @@ print("done loading")
 print(myfn)
 mybasenm <- basename(myfn)
 print(mybasenm)
-slice <- get(load(myfn))
+slice <- get(load(toString(myfn)))
+typeof(slice)
 
-splitix <- parallel::splitIndices(nx=length(slice), ncl=ceiling(length(slice) / 1))
+# splitix <- parallel::splitIndices(nx=length(slice), ncl=ceiling(length(slice) / 1))
 mm9Elements <- get(load("/u/ccha/ActiveDriverWGSR-genomeVariety/data/elementsmm9.RData"))
 
-mcres <- parallel::mclapply(splitix, function(x, ele) {
-  results = ActiveDriverWGS(mutations = mutations_df,
+mcres <- parallel::mclapply(1, function(x, ele) {
+  results = ActiveDriverWGS(mutations = ele,
                             elements = mm9Elements, 
                             reference = "mm9")
   return(results)
@@ -26,4 +27,4 @@ mcres <- parallel::mclapply(splitix, function(x, ele) {
 
 final <- ldply(mcres, data.frame)
 
-saveRDS(final, file=paste0("~/", gsub(mybasenm, pattern = ".rds", replacement="_comp.rds", fixed=TRUE)))
+saveRDS(final, file=paste0("~/complete/", gsub(mybasenm, pattern = ".rds", replacement="_comp.rds", fixed=TRUE)))
