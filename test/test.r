91d82b6c88b27e12563e2162735f1c4c8b005f10
Cathy <cathy455503@gmail.com>
2019-12-21 22:50:19 -0800

    push

--- test/test.r
+++ test/test.r
@@ -7,8 +7,14 @@ library(plyr)
 
 mm9Elements <- get(load("/u/ccha/ActiveDriverWGSR-genomeVariety/data/elementsmm9.RData"))
 
+print("done loading elements")
+
 slice <- get(load("/u/ccha/SB_Data/slice44.Rdata"))
 slice <- slice[!duplicated(slice),]
+
+print("done loading")
+pring("slice with number of rows:")
+print(length(slice))
   
 mcres <- parallel::mclapply(1, function(x, ele) {
   results = ActiveDriverWGS(mutations = ele,
@@ -17,6 +23,10 @@ mcres <- parallel::mclapply(1, function(x, ele) {
   return(results)
 },ele=slice, mc.cores=8)
 
+print("done ActiveDriver")
+
 final <- ldply(mcres, data.frame)
 
 save(final, file="/u/ccha/test/test.rdata")
+
+print("done saving rdata")
