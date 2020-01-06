5c6616018146dd2febb11cdef29a0ae16b45902d
Cathy <cathy455503@gmail.com>
2020-01-05 20:36:14 -0800

    push

---
+++ test/installpack.sh
@@ -0,0 +1,2 @@
+base="test"
+qsub -cwd -b y -N $base -l h_vmem=130g -o /dev/null -e /dev/null "module load R/3.6.0; LD_PRELOAD=/oicr/local/sw/gcc-6.5.0/lib64/libstdc++.so.6 Rscript /u/ccha/sleepingBeautyDataAnalysis/performADWGS.R  $file > /u/ccha/sleepingBeautyDataAnalysis/logs/$base.log"
