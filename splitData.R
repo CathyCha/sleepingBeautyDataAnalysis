b4b81375f820cc6524f866f6edb0668be0fd2aab
Cathy <cathy455503@gmail.com>
2020-01-15 14:32:40 -0500

    new element file

--- splitData.R
+++ splitData.R
@@ -1,14 +1,11 @@
 # Load the data 
-load("../SB_Data/SBData.Rdata")
+load("../element/mm9element.RData")
 
 # split() dataframe into 500 slices and stored as a large list of dataframes 
-splitted <- split(SBData, sample(rep(1:1000, 13400)))
+splitted <- split(mm9elements, sample(rep(1:300, 1600)))
 
 # save slices into separate Rdatas 
 for (i in 1:length(splitted)) {
   slice <- splitted[[i]]
   save(slice, file=paste0("../SB_Data/slice", i, ".Rdata"))
 }
-
-# #save as RData
-# save(splitted, file="splitted.Rdata")
