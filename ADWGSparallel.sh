010dffde1de50ecb95b7261ec99ed013b447e17d
Cathy <cathy455503@gmail.com>
2020-01-18 19:30:16 -0500

    fix

--- ADWGSparallel.sh
+++ ADWGSparallel.sh
@@ -2,7 +2,7 @@ dir=~/mm9elements/*
 
 for file in $dir; do
     base_ext=${file##*/}
-    base=${base_ext%.rds}
+    base=${base_ext%.Rdata}
     qsub -cwd -b y -N $base -l h_vmem=130g -o /dev/null -e /dev/null "LD_PRELOAD=/oicr/local/sw/gcc-6.5.0/lib64/libstdc++.so.6 Rscript /u/ccha/sleepingBeautyDataAnalysis/performADWGS.R  $file > /u/ccha/sleepingBeautyDataAnalysis/logs/$base.log"
     
 done
