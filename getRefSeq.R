4e8e9dc619f9acdc4dbdddc1bb191795f06db51c
Cathy <cathy455503@gmail.com>
2020-01-23 17:07:11 -0500

    fix

--- getRefSeq.R
+++ getRefSeq.R
@@ -12,6 +12,13 @@ print(mybasenm)
 slice <- get(load(toString(myfn)))
 print("done loading slice")
 
+#remove duplicates 
+slice <- slice[!duplicated(slice),]
+#remove non-legal chromosomes
+legal_chr = paste0("chr", c(1:19, "X", "Y"))
+slice <- slice[slice$chr %in% legal_chr,]
+
+
 #add ref seq to data frame 
 for (i in 1:length(slice$ref)) {
   slice$ref[i] <- toString(BSgenome::getSeq(BSgenome.Mmusculus.UCSC.mm9, 
