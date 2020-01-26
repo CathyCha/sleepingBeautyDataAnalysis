f8c2e38d3b8a5595ca27a5eed965e435710464b5
Cathy <cathy455503@gmail.com>
2020-01-26 16:32:14 -0500

    fix

--- performADWGS.R
+++ performADWGS.R
@@ -4,7 +4,7 @@ source("/u/ccha/ActiveDriverWGSR/R/fix_all_results.R")
 source("/u/ccha/ActiveDriverWGSR/R/format_muts.R")
 library(parallel)
 library(plyr)
-library(BSgenome.Mmusculus.UCSC.mm9)
+# library(BSgenome.Mmusculus.UCSC.mm9)
 
 #read in individual slice specified by cluster job
 myfn <- commandArgs(trailingOnly=TRUE)[1]
