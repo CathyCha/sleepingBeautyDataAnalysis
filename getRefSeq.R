f3305215c683e07958efd09da42f055e515ae55f
Cathy <cathy455503@gmail.com>
2020-01-29 17:56:02 -0500

    fix

--- getRefSeq.R
+++ getRefSeq.R
@@ -2,6 +2,7 @@ library(parallel)
 library(GenomicRanges)
 library(BSgenome.Mmusculus.UCSC.mm9)
 library(IRanges)
+library(BSgenome)
 
 myfn <- commandArgs(trailingOnly=TRUE)[1]
 print("done loading")
@@ -15,8 +16,8 @@ print("done loading slice")
 #add ref seq to data frame 
 for (i in 1:length(slice$ref)) {
   slice$ref[i] <- toString(BSgenome::getSeq(BSgenome.Mmusculus.UCSC.mm9, 
-                                            GenomicRanges::GRanges(slice$Chr[i], 
-                                                                   IRanges::IRanges(slice$pos2[i], slice$pos2[i]))))
+                                            GenomicRanges::GRanges(slice$chr[i], 
+                                                                   IRanges::IRanges(slice$pos1[i], slice$pos2[i]))))
 }
 save(slice, file=paste0("/u/ccha/finishedGetRef/", mybasenm))
 
