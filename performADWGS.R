248d039de5de57bb43d487fd70e3ff4db673da5b
Cathy <cathy455503@gmail.com>
2020-01-30 17:58:51 -0500

    fix

--- performADWGS.R
+++ performADWGS.R
@@ -4,7 +4,7 @@ source("/u/ccha/ActiveDriverWGSR/R/fix_all_results.R")
 source("/u/ccha/ActiveDriverWGSR/R/format_muts.R")
 library(parallel)
 library(plyr)
-# library(BSgenome.Mmusculus.UCSC.mm9)
+library(BSgenome.Mmusculus.UCSC.mm9)
 
 #read in individual slice specified by cluster job
 myfn <- commandArgs(trailingOnly=TRUE)[1]
@@ -16,12 +16,12 @@ slice <- get(load(toString(myfn)))
 print("done loading slice")
 
 # splitix <- parallel::splitIndices(nx=length(slice), ncl=ceiling(length(slice) / 1))
-SBData <- readRDS("/u/ccha/SB_Data/mouseBased.rds")
+SBData <- get(load("/u/ccha/SB_Data/mouseBasedGAlt.Rdata"))
 print("done loading SBData")
 
 mcres <- parallel::mclapply(1, function(x, ele) {
   results = ActiveDriverWGS(mutations = SBData,
-                            window_size = 300,
+                            window_size = 100,
                             elements = ele, 
                             reference = "mm9")
   return(results)
