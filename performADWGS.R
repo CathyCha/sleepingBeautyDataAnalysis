c300c26b004aea5751462cc0014b04f0ce725723
Cathy <cathy455503@gmail.com>
2020-02-26 20:12:25 -0500

    ws1000000

--- performADWGS.R
+++ performADWGS.R
@@ -21,7 +21,7 @@ print("done loading SBData")
 
 mcres <- parallel::mclapply(1, function(x, ele) {
   results = ActiveDriverWGS(mutations = SBData,
-                            window_size = 30000,
+                            window_size = 1000000,
                             elements = ele, 
                             reference = "mm9")
   return(results)
