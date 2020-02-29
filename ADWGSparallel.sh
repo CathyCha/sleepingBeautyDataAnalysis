8e3218603cdc739eb46a49e4edfec39ead052ae8
Cathy <cathy455503@gmail.com>
2020-02-29 14:57:19 -0500

    ws1mb

--- ADWGSparallel.sh
+++ ADWGSparallel.sh
@@ -1,8 +1,8 @@
-dir=~/mm9elements/ws100kb/*
+dir=~/mm9elements/ws1mb/*
 
 for file in $dir; do
     base_ext=${file##*/}
     base=${base_ext%.Rdata}
-    qsub -cwd -b y -N $base -l h_vmem=40g -o /dev/null -e /dev/null "module load R/3.6.0; LD_PRELOAD=/oicr/local/sw/gcc-6.5.0/lib64/libstdc++.so.6 Rscript /u/ccha/sleepingBeautyDataAnalysis/performADWGS.R  $file > /u/ccha/sleepingBeautyDataAnalysis/logs/$base.log"
+    qsub -cwd -b y -N $base -l h_vmem=40g -o /dev/null -e /dev/null "module load R/3.6.0; LD_PRELOAD=/oicr/local/sw/gcc-6.5.0/lib64/libstdc++.so.6 Rscript /u/ccha/sleepingBeautyDataAnalysis/performADWGS.R  $file > /u/ccha/sleepingBeautyDataAnalysis/logs/ws1mb/$base.log"
     
 done
