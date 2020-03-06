2886528a7e01bc24d48a6c59d2c1ab5e2655b55b
Cathy <cathy455503@gmail.com>
2020-03-06 15:19:42 -0500

    splitElement

--- splitElement.R
+++ splitElement.R
@@ -1,11 +1,11 @@
 # Load the data 
-load("/u/ccha/ActiveDriverWGSR/data/elementsmm9.RData")
+elementsmm9 <- readRDS("/u/ccha/element/mm9Ensembl.rds")
 
 # split() dataframe into 1000 slices and stored as a large list of dataframes 
-splitted <- split(elementsmm9, sample(rep(1:1000, 1150)))
+splitted <- split(elementsmm9, sample(rep(1:20, 1900)))
 
 # save slices into separate Rdatas 
 for (i in 1:length(splitted)) {
   slice <- splitted[[i]]
-  save(slice, file=paste0("../SB_Data/element/slice", i, ".Rdata"))
+  save(slice, file=paste0("/u/ccha/slices/ensemblElement/slice", i, ".Rdata"))
 }
