34411d347f06cf145a8f6c9712aa3439c110179a
Cathy <cathy455503@gmail.com>
2020-03-06 15:20:04 -0500

    collect slices

--- collectslice.R
+++ collectslice.R
@@ -1,7 +1,17 @@
-result = readRDS("/u/ccha/complete/slice1.rds")
+result = readRDS("/u/ccha/complete/ws1mb/slice1.rds")
 
-for (i in 2:300) {
-  slice = readRDS(paste0("/u/ccha/complete/slice", i , ".rds"))
+for (i in 869:1000) {
+  slice = readRDS(paste0("/u/ccha/complete/ws1mb/slice", i , ".rds"))
+  result = rbind(result, slice)
+} 
+
+saveRDS(result, "result.rds")
+
+
+result <- get(load("/u/ccha/complete/tumourBasedRData/slice1.rds"))
+
+for (i in 2:20) {
+  slice <- get(load(paste0("/u/ccha/complete/tumourBasedRData/slice", i , ".rds")))
   result = rbind(result, slice)
 } 
 
