c031049416a3650541e1e7c777d52484e75a0a6d
Cathy <cathy455503@gmail.com>
2020-03-08 15:46:02 -0400

    GetREf

--- getRefSeq.R
+++ getRefSeq.R
@@ -9,7 +9,7 @@ print("done loading")
 print(myfn)
 mybasenm <- basename(myfn)
 print(mybasenm)
-slice <- get(load(toString(myfn)))
+slice <- readRDS(toString(myfn))
 print("done loading slice")
 
 
@@ -18,6 +18,7 @@ for (i in 1:length(slice$ref)) {
   slice$ref[i] <- toString(BSgenome::getSeq(BSgenome.Mmusculus.UCSC.mm9, 
                                             GenomicRanges::GRanges(slice$chr[i], 
                                                                    IRanges::IRanges(slice$pos1[i], slice$pos2[i]))))
+  slice$alt[i] <- "GTTGAC"
 }
 save(slice, file=paste0("/u/ccha/finishedGetRef/", mybasenm))
 
