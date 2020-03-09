b7df7a195b63da29f276856e6f48e5070c938638
Cathy <cathy455503@gmail.com>
2020-03-09 15:08:37 -0400

    fix

--- ADWGSparallel.sh
+++ ADWGSparallel.sh
@@ -1,8 +1,8 @@
-dir=~/mm9elements/ws100kb/*
+dir=~/mm9elements/exonBased/*
 
 for file in $dir; do
     base_ext=${file##*/}
     base=${base_ext%.Rdata}
-    qsub -cwd -b y -N $base -l h_vmem=70g -o /dev/null -e /dev/null "module load R/3.6.0; LD_PRELOAD=/oicr/local/sw/gcc-6.5.0/lib64/libstdc++.so.6 Rscript /u/ccha/sleepingBeautyDataAnalysis/performADWGS.R  $file > /u/ccha/sleepingBeautyDataAnalysis/logs/ws1mb/$base.log"
+    qsub -cwd -b y -N $base -l h_vmem=70g -o /dev/null -e /dev/null "module load R/3.6.0; LD_PRELOAD=/oicr/local/sw/gcc-6.5.0/lib64/libstdc++.so.6 Rscript /u/ccha/sleepingBeautyDataAnalysis/performADWGS.R  $file > /u/ccha/sleepingBeautyDataAnalysis/logs/1tumourExon/$base.log"
     
 done
